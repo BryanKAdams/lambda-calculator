61a595365cdf359ea1f9bf4c3dc1c1987d83dbb8
BryanKAdams <17adamsbryk@washk12.org>
2019-11-05 17:20:13 -0700

    Looks like a calculator

--- src/components/ButtonComponents/NumberButtons/NumberButton.js
+++ src/components/ButtonComponents/NumberButtons/NumberButton.js
@@ -1,9 +1,16 @@
-import React from "react";
+import React, {useState} from "react";
+import {Display} from "../../DisplayComponents/Display"
 
-const NumberButton = () => {
+export const NumberButton = props => {
+  const [number, changeNumber] = useState("0");
   return (
     <>
       {/* Display a button element rendering the data being passed down from the parent container on props */}
+      <button className="number_button">
+        <span role="img" aria-label="heart">
+          {props.button}
+        </span>
+      </button>
     </>
   );
 };
